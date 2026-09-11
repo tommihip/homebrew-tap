53f2588a7525d8f5ad0525fee7233da689d29297
Tommi Hippelainen <tommi@hippelainen.com>
2026-09-11 01:13:45 +0100

    termsie 0.6.0

--- Casks/termsie.rb
+++ Casks/termsie.rb
@@ -1,6 +1,6 @@
 cask "termsie" do
-  version "0.5.0"
-  sha256 "42fd8b081107348ce9392753eb6e3055b2fa3b980b3e7caa2a694b93e0623ebb"
+  version "0.6.0"
+  sha256 "f07ae967704d567c6d7b4e2217abb85e2d5ccd06bbef3fd04822023e9da2e2c8"
 
   url "https://github.com/tommihip/termsie/releases/download/v#{version}/Termsie-#{version}.dmg"
   name "Termsie"
